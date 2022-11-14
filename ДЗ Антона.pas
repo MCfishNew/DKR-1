a731364e51b23df3fdaf575c3b032b8067d54c30
MCfishNew <103477341+MCfishNew@users.noreply.github.com>
2022-11-14 16:28:06 +0300

    Add files via upload

---
+++ ДЗ Антона.pas
@@ -0,0 +1,15 @@
+﻿var x: real; 
+var y:real;
+begin
+ x:=-10;
+  while x<=6 do
+  begin
+  if x <-8 then y:= power(x,3)+power(e,x)
+  else if x<-3 then y:= sqr(x)-power(x,3)
+  else if x<4 then y:=cos(2*x)*cos(x)-sin(x)
+  else y:=47/x;  
+  x:=x+0.2;
+  writeln (y:0:2);
+  end;
+ 
+end.
